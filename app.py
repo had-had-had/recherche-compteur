347edb9a3a5ad7268812425b910e55b5143b81a4
had-had-had <yess.hadrich@gmail.com>
2026-03-18 11:55:50 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
-import os
-
 file = os.path.join(os.path.dirname(__file__), "data.xlsx")
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    df = pd.read_excel(file)
-
-    search = request.form.get("numctr")
-
-    if search:
-        df = df[df["NUM_CTR"].astype(str).str.contains(search)]
-
-    data = df.to_dict(orient="records")
-
-    return render_template("index.html", data=data)
+    return str(os.listdir())
 
 @app.route("/update", methods=["POST"])
 def update():
@@ -26,9 +16,7 @@ def update():
     new_index = request.form.get("index")
 
     df = pd.read_excel(file)
-
     df.loc[df["NUM_CTR"].astype(str) == num, "INDEX_DEPS"] = new_index
-
     df.to_excel(file, index=False)
 
     return redirect("/")
